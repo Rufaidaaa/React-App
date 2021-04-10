1696b788de762660b6ff41c439f2c72d75125324
Rufaidashafiq <rufaidashafik@gmail.com>
2021-04-11 03:48:49 +0500

    Rendering classes dynamically

---
+++ src/components/Counter.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = { 
+        count:0
+     }
+
+     styles={
+         fontSize: 20
+     }
+    render() { 
+      
+        return (  
+            <>
+            <span style={this.styles} className={this.getbageClasses()}>{this.format()}</span>
+            <button className="btn btn-danger">increment </button>
+            </>
+        );
+    }
+
+    getbageClasses() {
+        let classes = " badge m-2 badge-";
+        classes += this.state.count === 0 ? 'warning' : 'primary';
+        return classes;
+    }
+
+    format(){
+      const { count } =this.state
+      return count === 0 ? "zero" : count
+    }
+}
+ 
+export default Counter;
